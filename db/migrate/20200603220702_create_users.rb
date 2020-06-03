a1ffb45fbbdd9ba6f4d57c8d23e4e89c6c66cc6e
isabel k. lee <hello@isabelklee.co>
2020-06-03 18:07:32 -0400

    created user model

---
+++ db/migrate/20200603220702_create_users.rb
@@ -0,0 +1,11 @@
+class CreateUsers < ActiveRecord::Migration[6.0]
+  def change
+    create_table :users do |t|
+      t.string :username
+      t.string :email_address
+      t.datetime :join_date
+
+      t.timestamps
+    end
+  end
+end
